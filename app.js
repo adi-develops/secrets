d279d364882d62dd6b2a8d1722a67feff7750a2b
Aditya Agarwal <aditya.agarwal1311@gmail.com>
2022-10-07 01:47:31 +0530

    Level 4 Salting and Hashing

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express()
 const port = 3000
 const ejs = require("ejs")
 const mongoose = require('mongoose')
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 mongoose.connect('mongodb://localhost:27017/userDB')
@@ -32,36 +33,44 @@ app.get('/register', (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    const userItem = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        const userItem = new User({
+            email: req.body.username,
+            password: hash
+        })
+
+        userItem.save((err) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                res.render("secrets")
+            }
+        })
     })
 
-    userItem.save((err) => {
-        if(err){
-            console.log(err)
-        }
-        else {
-            res.render("secrets")
-        }
-    })
 })
 
 app.post("/login", (req, res) => {
 
-    User.findOne({email: req.body.username}, (err, foundUser) => {
-        if(err){
-            console.log(err)
+    User.findOne({ email: req.body.username }, (err, foundUser) => {
+        if (err) {
+            console.log("findOne error:", err)
         }
-        else{
-            if(foundUser){
-                if(md5(req.body.password) === foundUser.password){
-                    res.render("secrets")
+        else {
+            bcrypt.compare(req.body.password, foundUser.password, (err, result) => {
+                if(!err){
+
+                    if (result === true) {
+                        res.render("secrets")
+                    }
+                    else {
+                        res.send("User Not Found")
+                    }
                 }
-            }
-            else{
-                res.send("User Not Found")
-            }
+            })
         }
     })
 })
